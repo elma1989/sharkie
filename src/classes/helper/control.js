aed5a233efcce787c7fa96e5af9a64ac95982950
Marco Elste <marco.elste@web.de>
2026-06-07 16:22:54 +0200

    feat: Logic Control / Keyboard
       - Added control.js:
          - Added class Control
          - Implemented method setCtrl(name, state)
    
       - Added keyboard.js:
          - Added class Keyboard
          - Added private methods addKeyDownListener(), addKeyUpListener()

---
+++ src/classes/helper/control.js
@@ -0,0 +1,20 @@
+export class Control {
+    #controls = {
+        up: false,
+        right: false,
+        down: false,
+        left: false,
+        attack: false,
+    }
+
+    get ctrl() { return this.#controls; }
+
+    /**
+     * Sets controls.
+     * @param {string} name - Name of control
+     * @param {boolean} state - True for active.
+     */
+    setCtrl(name, state) {
+        if (name in this.#controls) this.#controls[name] = state;
+    }
+}
