4b1dd91ed582fabe5ecd485af62430a72281379c
Marco Elste <marco.elste@web.de>
2026-06-17 12:59:43 +0200

    feat: Logic Coin
       - Added coin.js:
          - Added class Coin(x,y)
          - Overridden load(), collect(sharkie)
    
       - Updated sharkie.js:
          - Added porperty coins:number
          - Implemented addCoin()
    
       - Updated level.js:
           - Modified private method createCollectables - Added 20 coins

---
+++ src/classes/model/coin.js
@@ -0,0 +1,42 @@
+import { Collectable } from "../abstract/collectable.js";
+import { ImgHelper } from "../helper/img-helper.js";
+import { Sharkie } from "./sharkie.js";
+
+/**
+ * @typedef {import('../types.js').Offset} Offset
+ * @typedef {import('../types.js').Animation} Animation
+ */
+
+/** Represents a coin to money on collect. */
+export class Coin extends Collectable {
+    /**
+     * Creates a coin.
+     * @param {number} x - X-Pos of object.
+     * @param {number} y - Y-Pos of object.
+     */
+    constructor(x, y) {
+        super(x, y, 99, 93, {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20
+        }, {
+            'idle': {
+                frames: [],
+                duration: 1500,
+                loop: true
+            }
+        });
+    }
+
+    async load() {
+        this.img = await this.loadImage(ImgHelper.url(ImgHelper.COLLECTABLE.coin[0]));
+        this.animations.idle.frames = await this.loadAnimations(ImgHelper.urls(ImgHelper.COLLECTABLE.coin));
+    }
+
+    collect(sharkie) {
+        if (!sharkie instanceof Sharkie) return;
+        sharkie.addCoin();
+        this.onCollect?.();
+    }
+}
