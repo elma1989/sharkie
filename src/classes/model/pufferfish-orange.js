dcbc2e4c511a64fd37792d9461d1c8d0d8d9f5be
Marco Elste <marco.elste@web.de>
2026-07-03 10:32:05 +0200

    feat: Logic orange puffer fish
       - Added bufferfish-orange.js:
          - Added class OrangeBufferFish(x,y,minX,maxX)
          - Overridden async load()

---
+++ src/classes/model/pufferfish-orange.js
@@ -0,0 +1,26 @@
+import { PufferFish } from "../abstract/puffer-fish.js";
+import { ImgHelper } from "../helper/img-helper.js";
+
+/** A puffer fish, which is orange. */
+export class OrangePufferFish extends PufferFish {
+    /**
+     * Creates a orange puffer fish.
+     * @param {number} x - X-Pos of oubject.
+     * @param {number} y - Y-Pos of object.
+     * @param {number} minX - Minimum X-Pos of object.
+     * @param {number} maxX - Maximum X-Pos of object.
+     */
+    constructor(x, y, minX, maxX) {
+        super(x, y, minX, maxX);
+    }
+
+    async load() {
+        this.img = await this.loadImage(ImgHelper.url(ImgHelper.ENEMY["pufferfish/orange/swim/empty"][0]));
+        this.deathImg.empty = await this.loadImage(ImgHelper.url(ImgHelper.ENEMY["pufferfish/orange/dead/empty"]));
+        this.deathImg.transition = await this.loadImage(ImgHelper.url(ImgHelper.ENEMY["pufferfish/orange/dead/transition"]));
+        this.deathImg.full = await this.loadImage(ImgHelper.url(ImgHelper.ENEMY["pufferfish/orange/dead/full"]));
+        this.animations['swim/empty'].frames = await this.loadAnimations(ImgHelper.urls(ImgHelper.ENEMY["pufferfish/orange/swim/empty"]));
+        this.animations['swim/transition'].frames = await this.loadAnimations(ImgHelper.urls(ImgHelper.ENEMY["pufferfish/orange/swim/transition"]));
+        this.animations['swim/full'].frames = await this.loadAnimations(ImgHelper.urls(ImgHelper.ENEMY["pufferfish/orange/swim/full"]));
+    }
+}
