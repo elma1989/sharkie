988e8be26f2d1cde3e6406763035fb841dd34e98
Marco Elste <marco.elste@web.de>
2026-07-25 19:12:19 +0200

    chore: Sound-Insertion
       - Added files for sound of game
       - Added sound.js - Added all paths

---
+++ src/classes/helper/sounds.js
@@ -0,0 +1,15 @@
+export const sounds = {
+    music: 'music.mp3',
+    approach: 'approach.mp3',
+    skill: 'skill.mp3',
+    'attack/slap': 'attack-slap.mp3',
+    'attack/bubble': 'attack-bubble.mp3',
+    'collect/coin': 'coin.mp3',
+    'collect/poison': 'jar.mp3',
+    'hurt/poison': 'hurt-poison.mp3',
+    'hurt/electric': 'hurt-electric.mp3',
+    'hurt/bubble': 'hurt-bubble.mp3',
+    'hurt/orca': 'hurt-orca.mp3',
+    win: 'win.mp3',
+    lose: 'lose.mp3'
+}
