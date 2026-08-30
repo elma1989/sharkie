0e51ef39e5eb8d77081ac9e2ae37fd4b3c725553
Marco Elste <marco.elste@web.de>
2026-08-30 10:01:48 +0200

    ref: HTMLCurstomElemnt
       - Removed abstract/custom-elemnt.js
       - Remvoed ui/html-custom.js
       - Upadated canvas.js: Removed base class

--- src/classes/ui/canvas.js
+++ src/classes/ui/canvas.js
@@ -1,16 +1,19 @@
 import { GameConfig } from "../game-config.js";
-import { HTMLCustomElement } from "../abstract/custom-element.js";
 
-export class Canvas extends HTMLCustomElement {
-    #ctx;
+/** Canvas-Elment, on which game will be drawn. */
+export class Canvas {
+    /**
+     * Context of canvas.
+     * @type {CanvasRenderingContext2D?}
+     */
+    #ctx = null;
 
     constructor() {
-        super('canvas');
-        const canvas = this.element;
-        if (canvas) {
-            canvas.width = GameConfig.WIDTH;
-            canvas.height = GameConfig.HEIGHT;
-            this.#ctx = canvas.getContext('2d');
+        const element = document.getElementById('canvas');
+        if (element) {
+            this.#ctx = element.getContext('2d');
+            element.width = GameConfig.WIDTH;
+            element.height = GameConfig.HEIGHT;
         }
     }
 
